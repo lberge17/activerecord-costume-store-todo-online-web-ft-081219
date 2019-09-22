044d787a8d31f7a5fb52a2184226240d4902a8c1
Laura Berge <lauraeberge@gmail.com>
2019-09-22 23:57:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,5 +15,7 @@ class Costume < ActiveRecord::Migration[5.1]
       t.integer :price
       t.size :string
       t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at
     end
 end
