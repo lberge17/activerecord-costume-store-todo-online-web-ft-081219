026f95bedb015747e719655040a5b59ff3016f4f
Laura Berge <lauraeberge@gmail.com>
2019-09-23 00:08:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :string
+      t.string :size
       t.string :image_url
       t.timestamps
     end
